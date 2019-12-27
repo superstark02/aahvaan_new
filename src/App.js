68ebc1075ce3b736fecf2d319cf5caddccb35b47
Sundeep Chand <sundeep.chand2001@gmail.com>
2019-12-27 23:57:29 +0530

    Updated UI

--- src/App.js
+++ src/App.js
@@ -46,6 +46,44 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
+  avatar: {
+    color: 'white',
+    backgroundColor: 'gray',
+    border: '4px solid black',
+    padding: '12px',
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    marginTop: '15px',
+    marginBottom: '14px',
+  },
+  navBar: {
+    padding: '13px',
+    backgroundColor: 'black',
+    opacity: '0.76',
+    height: '10vh',
+    zIndex: '1',
+  },
+  navButtonContact: {
+    position: 'absolute',
+    top: '2.5vh',
+    minTop: '10px',
+    right: '18px',
+    color: 'gold',
+    opacity: '1',
+    fontSize: '20px',
+    zIndex: '2',
+  },
+  navButtonAbout: {
+    position: 'absolute',
+    top: '2.5vh',
+    minTop: '10px',
+    right: '183px',
+    color: 'gold',
+    opacity: '1',
+    fontSize: '20px',
+    zIndex: '2',
+  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
@@ -137,15 +175,21 @@ export default function ButtonAppBar() {
 
   return (
     <div className="app">
-      <div className={classes.root}>
-        <AppBar position="sticky" color='secondary'>
-          <Toolbar>
-            <Typography variant="h5" className={classes.title} align='center'>
-              AAHVAAN
-            </Typography>
-          </Toolbar>
+
+      {/*---------Navbar----------------*/}
+      <div>
+        <AppBar className={classes.navBar} position="sticky" color="secondary">
         </AppBar>
       </div>
+      <div id="navbar-content">
+        <Typography className={classes.root}>
+          <Button className={classes.navButtonAbout} href="">About</Button>
+          <Button className={classes.navButtonContact} href="">Contact Us</Button>
+        </Typography>
+      </div>
+      {/*-------------------------------*/}
+
+      {/*----------Overlay--------------*/}
       <div className="overlay-text-container" align="center">
         <h1 className="overlay-text">
           AAHVAAN
@@ -170,6 +214,38 @@ export default function ButtonAppBar() {
           ))}
         </Gallery>
       </Paper>
+      {/*-------------------------------*/}
+
+      {/*-----------About section-------------*/}
+      <div id="about">
+        <Paper>
+          <Card>
+            <AppBar position="sticky" color='secondary'>
+              <Toolbar>
+                <Typography variant="h5" className={classes.title} align='center'>
+                  ABOUT
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at convallis dui, hendrerit 
+              pharetra justo. Duis maximus, dui vitae suscipit lobortis, ipsum diam ultrices quam, vel suscipit 
+              ipsum nibh in orci. Curabitur vitae augue eleifend tortor varius varius. Etiam non elementum leo. 
+              Phasellus id metus non est mattis ultrices. Praesent vel metus eget velit mollis dictum ac sed nunc. 
+              Phasellus mattis, orci sed dictum malesuada, nulla nulla fringilla mauris, at convallis lacus 
+              metus vel risus. In sagittis porttitor pharetra.
+              Morbi ut pulvinar metus. Suspendisse quis risus vel ligula dictum volutpat et at dui. Nulla 
+              vestibulum dapibus turpis eget feugiat. Suspendisse erat nibh, blandit eget sodales a, 
+              fermentum a velit. Nulla consequat iaculis enim eget tincidunt. Pellentesque sapien ipsum, 
+              ultricies sodales est non, sollicitudin malesuada erat. Nunc vulputate tempus venenatis. 
+              Phasellus tincidunt dignissim enim vitae pretium. Ut tristique, massa a tristique viverra, 
+              purus nulla pretium metus, et mattis nisl magna ac lorem. Donec et gravida dolor. Phasellus 
+              facilisis risus nec magna vestibulum, et auctor orci maximus.
+            </div>
+          </Card>
+        </Paper>
+      </div>
+      {/*-------------------------------------*/}
 
       {/*---------Form Components-------------*/}
       <div className={classes.root}>
@@ -187,15 +263,21 @@ export default function ButtonAppBar() {
       <Grid container spacing={3}>
         <Grid item xs={6}>
           <Paper className={classes.paper} align="center">
+            <div className={classes.avatar}>
+              <FontAwesomeIcon icon={faUser} />
+            </div>
             <Button component={ Link } to="/single" variant="contained" color="secondary">
-              <FontAwesomeIcon icon={faUser} />&nbsp;Single
+              Single
             </Button>
           </Paper>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper} align="center">
+            <div className={classes.avatar}>
+              <FontAwesomeIcon icon={faUsers} />
+            </div>
             <Button component={ Link } to="/team" variant="contained" color="secondary">
-              <FontAwesomeIcon icon={faUsers} />&nbsp;Team
+              Team
             </Button>
           </Paper>
         </Grid>
@@ -209,11 +291,13 @@ export default function ButtonAppBar() {
             Our Sponsers
           </Typography>
           <Typography variant="h6" align="center">
-            <FontAwesomeIcon icon={faAdobe} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faAmazon} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faApple} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faGoogle} />&nbsp;&nbsp;
-            <FontAwesomeIcon icon={faMicrosoft} />&nbsp;&nbsp;
+            <div class="sponsors">
+              <FontAwesomeIcon icon={faAdobe} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faAmazon} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faApple} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faGoogle} />&nbsp;&nbsp;
+              <FontAwesomeIcon icon={faMicrosoft} />&nbsp;&nbsp;
+            </div>
           </Typography>
         </Card>
       </Paper>
