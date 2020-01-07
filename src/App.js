796b26976c59d6523a0bebfe261d538a8e787693
superstark02 <54995058+superstark02@users.noreply.github.com>
2020-01-07 19:57:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -174,9 +174,10 @@ const useStyles = makeStyles(theme => ({
   upper: {
     backgroundColor: 'lightgray',
     color: 'black',
-    padding: '10px',
+    padding: '2%',
     display: 'flex',
     alignItems: 'stretch',
+    height: '20vh',
   },
   lower: {
     textAlign: 'center',
@@ -278,7 +279,7 @@ export default function ButtonAppBar() {
 
       <Grid container spacing={3} className={classes.content}>
         <Grid item xs={6}>
-          <Paper className={classes.paper} align="center">
+          <Paper className={classes.paper} align="center" >
             <div className={classes.avatar}>
               <FontAwesomeIcon icon={faUser} />
             </div>
